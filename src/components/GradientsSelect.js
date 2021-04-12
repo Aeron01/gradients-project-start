de83533553e93ce535be3f92d57d78deae9672bd
Aeron01 <one.soulreaper@gmail.com>
2021-04-12 23:45:53 +0200

    ok

--- src/components/GradientsSelect.js
+++ src/components/GradientsSelect.js
@@ -1,17 +1,14 @@
-//import { gradients } from "../gradients"
+
 import { uniqueTags } from "../gradients"
 
 const GradientsSelect = () => {
-  const
   return (
     <>
       <h2 className="text-center my-4">Alyra Gradients</h2>
       <div className="input-group mb-3">
         <label className="input-group-text mb-4" for="select">Filter par tag</label>
         <select className="form-control mb-4">
-          <option>
-            Tous
-          </option>
+          <option>Tous</option>
           {uniqueTags.map(color => <option>{color}</option>)}
         </select>
       </div>
